c610c025233693a47859dcfa956a3fef694d869d
Khanh Tran <martinngo@goappable.com>
2020-06-29 09:44:55 +0700

    udpate

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,31 @@
 import React from "react";
 import TodoForm from "../todo-form/TodoForm";
 import TodoList from "../todo-list/TodoList";
+
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    // Init st
     this.state = { todoList: [], filterCriteria: "all" };
   }
+
   componentDidMount() {
-    this.setState({todoList: this.getTodoList()});
+    // console.log('componentDidMount', window.renderTime )
+    // this.setState({todoList: this.getTodoList()});
   }
+
   getTodoList = () => {
-    return JSON.parse(localStorage.getItem("todo-list")) || [];
+    const todos = localStorage.getItem("todo-list")
+    return todos ? JSON.parse(todos) : [];
   };
 
+  // cb
   filterTodo = (filterCriteria) => {
     this.setState({ filterCriteria });
   };
 
+  // cb
   addTodo = (value) => {
     if (!value) return;
 
@@ -49,24 +58,27 @@ class App extends React.Component {
   };
 
   render() {
+    window.renderTime = window.renderTime + 1;
     const criteria = this.state.filterCriteria;
-    console.log('2 lan');
+    console.log('render',window.renderTime)
 
-    const todoList =
-      criteria === "all"
-        ? this.state.todoList
-        : this.state.todoList.filter((x) =>
-            criteria === "completed" ? x.isCompleted : !x.isCompleted
-          );
+    // const todoList =
+    //   criteria === "all"
+    //     ? this.state.todoList
+    //     : this.state.todoList.filter((x) =>
+    //         criteria === "completed" ? x.isCompleted : !x.isCompleted
+    //       );
+
     return (
-      <>
-        <TodoForm onAdd={this.addTodo} onFilter={this.filterTodo} />
-        <TodoList
-          todoList={todoList}
-          onToggleSTT={this.toggleSTT}
-          onRemoveTodo={this.removeToDo}
-        />
-      </>
+      <h1>Debug</h1>
+      // <>
+      //   <TodoForm onAdd={this.addTodo} onFilter={this.filterTodo} />
+      //   <TodoList
+      //     todoList={todoList}
+      //     onToggleSTT={this.toggleSTT}
+      //     onRemoveTodo={this.removeToDo}
+      //   />
+      // </>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './components/app/App'
 import * as serviceWorker from './serviceWorker';
 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),()=>{console.log('ReactDOM', window.renderTime)}
 );
 
 // If you want your app to work offline and load faster, you can change
